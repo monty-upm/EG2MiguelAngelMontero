790c2b97bf32f44066ec627603e9f2d4bfe1ea12
Rovi23 <roviyo23@gmail.com>
2013-10-18 20:41:26 +0200

    finalizacion del metodo esPrimo de NumeroEntero

--- src/spai/egit2/NumeroEntero.java
+++ src/spai/egit2/NumeroEntero.java
@@ -1,38 +1,47 @@
 package spai.egit2;
 
 public class NumeroEntero {
-    private int valor;
+	private int valor;
 
-    public NumeroEntero(int valor) {
-        this.valor = valor;
-    }
+	public NumeroEntero(int valor) {
+		this.valor = valor;
+	}
 
-    public NumeroEntero() {
-        this(0);
-    }
+	public NumeroEntero() {
+		this(0);
+	}
 
-    public int getValor() {
-        return valor;
-    }
+	public int getValor() {
+		return valor;
+	}
 
-    private void setValor(int valor) {
-        this.valor = valor;
-    }
+	private void setValor(int valor) {
+		this.valor = valor;
+	}
 
-    public void incrementar() {
-        this.setValor(this.valor + 1);
-    }
+	public void incrementar() {
+		this.setValor(this.valor + 1);
+	}
 
-    public void doblar() {
-        this.setValor(this.valor * 2);
-    }
+	public void doblar() {
+		this.setValor(this.valor * 2);
+	}
 
-    public void decrementar() {
-        this.valor--;
-    }
-    
-    public boolean esPrimo(int integer){
-    	return false;
-    }
+	public void decrementar() {
+		this.valor--;
+	}
 
+	public boolean esPrimo(int numero) {
+
+		if(numero==1) // el 1 no se considera primo
+			return false;
+		int aux;
+		for (int cont = 2; cont < numero; cont++) {
+			aux = numero % cont;
+			if (aux == 0)
+				return false;
+		}
+		return true;
+
+	}
 }
